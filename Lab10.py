d3b9baf0f70da598e6b6507a194e360341c4098f
AnasAbbadi2003 <abba0146@algonquinlive.com>
2025-03-24 23:39:07 -0400

    Done

--- Lab10.py
+++ Lab10.py
@@ -1,3 +1,5 @@
+# import time
+# from adafruit_circuitplayground import cp
 # Dictionary of letters and their translation to morse code
 morse_code = {
     'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 
@@ -8,17 +10,17 @@ morse_code = {
     'Z': '--..',
     '0': '-----', '1': '.----', '2': '..---', '3': '...--', 
     '4': '....-', '5': '.....', '6': '-....', '7': '--...', 
-    '8': '---..', '9': '----.', ' ':'//'
+    '8': '---..', '9': '----.', ' ':'/'
 }
 # The user Input
 user_phrase = input("Enter your phrase : ")
 # Function to clean the user input into only the wanted characters
 def check_phrase(user_phrase , morse):
-    test = True
-    while test:
+    test01 = True
+    while test01:
         # string that will have the cleaned version
         cleaned = ''   
-        # check if the user have lest the input empty
+        # check if the user have left the input empty
         if(len(user_phrase) == 0):
             print("Please don't leave your input empty")
             user_phrase = input("Enter your phrase : ") 
@@ -26,10 +28,11 @@ def check_phrase(user_phrase , morse):
             for l in user_phrase.upper():    #looping over the input with making it upperCase to match the dictionary
                 if l in morse :
                     cleaned += l
-            test = False    # to pause the loop
+            test01 = False    # to pause the loop
             return cleaned 
+#putting the return value into a variable
 cleaned = check_phrase(user_phrase,morse_code)
-
+print("The cleaned version is : ",cleaned)
 # Function to translate the input into a list of morse 
 def translate (cleaned,morse_code):
         morse_list = []
@@ -37,4 +40,10 @@ def translate (cleaned,morse_code):
             morse_list.append(morse_code[i])
         return morse_list
 
-translate(cleaned,morse_code)
+#putting the return value into a variable
+morse_List=translate(cleaned,morse_code)
+print("The morse code translation is : ",morse_List)
+  
+
+
+    
